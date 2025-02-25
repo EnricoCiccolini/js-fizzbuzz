29e70cb201676cae9595d505d490aad1b672fe4a
EnricoCiccolini <piroroma90@gmail.com>
2025-02-25 14:35:00 +0100

    first commit

---
+++ info.md
@@ -0,0 +1,21 @@
+##  js-fizzbuzz
+
+# Scrivi un programma che stampi i numeri da 1 a 100,
+# ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+# Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
+procedimento 
+
+creremo due variabili div5 e div3
+apriamo un ciclo con contatore a 1  finoa che non sia maggiore uguale a 100 icrementando ogni volta il contatore di uno
+
+faremo due calcoli per capire il contatore è divisibile per 3 e/o 5
+SE divisibile per 3 e 5 
+    -stamperemo in console  fizz buz 
+se divisibile per 3 
+    - stamperemo in console    fiz 
+se divisibile per 5 
+    - stamperemo in console buz
+altrimenti 
+    stamperemo in console la variabile contatore         
---
+++ js/script.js
@@ -0,0 +1,30 @@
+// dichiarazione variabili 
+let div5 = 0
+let div3 = 0
+
+// ciclo di conteggio fino a 100
+for(let i=1 ; i <= 100; i++){
+    
+    // calcolo per controllare che siano divisibili per le variabili
+    div3 = (i % 3)
+    div5 =  (i % 5) 
+
+    // prima if se il numero e divisibile per tutti e due gli opratori 
+    if (div3 === 0 && div5 === 0 ){
+        console.log (`fix bux`)
+    }
+
+    // seconda if se il numero e divisibile solo per il primo 
+    else if ( div3 === 0){
+        console.log (`fix `)
+    }
+
+    // seconda if se il numero e divisibile solo per il secondo 
+    else if ( div5 === 0 ){
+        console.log (`bux`)
+    }
+    // altrimenti se non e divisibile per nessun operatore 
+    else{
+        console.log (i)
+    }
+}
